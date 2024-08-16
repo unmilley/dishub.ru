6a96ce335f1ee0c39fb461efff2ad381d8cee489
unmilley <1337@milley.uno>
2024-08-16 16:41:26 +0500

    `feat: added global styles and colors`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,27 @@ import type { Theme } from './types'
 
 const dark: Theme = {
   ...themes.dark,
+  'base-100': '#191919',
+  'base-200': '#222222',
+  'base-300': '#232323',
+  'base-content': '#77787a',
+  primary: '#0077FF',
+  'primary-content': '#ffffff',
 }
 
 const light: Theme = {
   ...themes.light,
+  'base-100': '#ffffff',
+  'base-200': '#ebecf0',
+  'base-300': '#f5f5f5',
+  'base-content': '#9ba1ad',
+  primary: '#0077FF',
+  'primary-content': '#ffffff',
 }
 
 const config: Partial<Config> = {
   content: ['./components/**/*.vue', './layouts/**/*.vue', './pages/**/*.vue', './app.vue', './error.vue'],
-  darkMode: ['class', 'data-theme'],
+  darkMode: ['class'],
   theme: {
     extend: {},
   },
