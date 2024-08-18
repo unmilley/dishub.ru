ebfb94f16cb8719289c30ef43c4172c670051d11
unmilley <1337@milley.uno>
2024-08-19 00:28:02 +0500

    `feat: added some ui components`

---
+++ components/ui/tooltip.vue
@@ -0,0 +1,34 @@
+<template>
+  <component :is="tag" :data-tip="tip" :class="['tooltip', color, position, open]">
+    <slot />
+  </component>
+</template>
+
+<script lang="ts" setup>
+type Props = {
+  tag?: 'div' | 'span' | 'p'
+  color?: 'primary' | 'base-100' | 'base-200'
+  position?: 'top' | 'bottom' | 'left' | 'right'
+  tip: string
+  open?: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), { tag: 'div', open: false })
+
+const color = computed(() => {
+  if (props.color === 'primary') return 'tooltip-primary'
+  else if (props.color === 'base-100') return 'tooltip-base-100'
+  else if (props.color === 'base-200') return 'tooltip-base-200'
+  return ''
+})
+
+const position = computed(() => {
+  if (props.position === 'bottom') return 'tooltip-bottom'
+  if (props.position === 'top') return 'tooltip-top'
+  if (props.position === 'left') return 'tooltip-left'
+  if (props.position === 'right') return 'tooltip-right'
+  return ''
+})
+
+const open = computed(() => (props.open ? 'tooltip-open' : ''))
+</script>
